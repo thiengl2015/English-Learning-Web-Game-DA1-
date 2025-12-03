44d42ebfe2c084868f676d7fc21b75e0b62962a5
thiengl2015 <giathien899@gmail.com>
2025-12-03 23:01:56 +0700

    feat: add UserUnitProgress model

---
+++ backend/src/models/UserUnitProgress.js
@@ -0,0 +1,56 @@
+module.exports = (sequelize, DataTypes) => {
+  const UserUnitProgress = sequelize.define(
+    "UserUnitProgress",
+    {
+      user_id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        references: {
+          model: "users",
+          key: "id",
+        },
+      },
+      unit_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        references: {
+          model: "units",
+          key: "id",
+        },
+      },
+      progress: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      crown_level: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      unlocked: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      completed_at: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+    },
+    {
+      tableName: "user_unit_progress",
+      timestamps: false,
+    }
+  );
+
+  UserUnitProgress.associate = (models) => {
+    UserUnitProgress.belongsTo(models.User, {
+      foreignKey: "user_id",
+      as: "user",
+    });
+    UserUnitProgress.belongsTo(models.Unit, {
+      foreignKey: "unit_id",
+      as: "unit",
+    });
+  };
+
+  return UserUnitProgress;
+};
